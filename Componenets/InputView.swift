a0b8383e002638d62bd94d126b0bfc55616473ed
Mina Mansour <manoonmina3@gmail.com>
2024-07-11 17:30:50 -0400

    log-in-view and input view

---
+++ Componenets/InputView.swift
@@ -0,0 +1,17 @@
+//
+//  input_view.swift
+//  HopSpot.
+//
+//  Created by Mina on 2024-07-11.
+//
+
+import SwiftUI
+
+struct input_view: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+        
+    }
+
