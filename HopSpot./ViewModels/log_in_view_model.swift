f0e7b2abdea09fa8e0f6f6441f8f81de1fbf7fdb
Benjamin Roman Perez <59741853+roman20203@users.noreply.github.com>
2024-07-10 20:56:57 -0400

    Started back end for log in and register views, created temp launch screen

---
+++ HopSpot./ViewModels/log_in_view_model.swift
@@ -0,0 +1,8 @@
+//
+//  log_in_view_model.swift
+//  HopSpot.
+//
+//  Created by Ben Roman on 2024-07-10.
+//
+
+import Foundation
---
+++ HopSpot./ViewModels/register_view_model.swift
@@ -0,0 +1,8 @@
+//
+//  register_view_model.swift
+//  HopSpot.
+//
+//  Created by Ben Roman on 2024-07-10.
+//
+
+import Foundation
