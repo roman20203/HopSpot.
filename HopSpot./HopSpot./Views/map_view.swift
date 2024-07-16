e1dbfacb3f05d44a32bcc5f5dc74a5708ea8bf63
Mina Mansour <manoonmina3@gmail.com>
2024-07-15 20:04:24 -0400

    test

---
+++ HopSpot./HopSpot./Views/map_view.swift
@@ -0,0 +1,29 @@
+//
+//  map_view.swift
+//  HopSpot.
+//
+//  Created by Ben Roman on 2024-07-05.
+//
+
+import MapKit
+import SwiftUI
+
+struct map_view: View {
+    var body: some View{
+        Map()
+        
+    }
+    
+    
+    
+    
+    
+    
+
+
+    
+}
+
+#Preview {
+    map_view()
+}
