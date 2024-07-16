5244151882d10c404f48e990c27ff2e2f1500ab0
Mina Mansour <manoonmina3@gmail.com>
2024-07-15 20:09:35 -0400

    small changes to log_in_view_model, register_view_model

--- HopSpot./ViewModels/log_in_view_model.swift
+++ HopSpot./ViewModels/log_in_view_model.swift
@@ -2,33 +2,36 @@
 //  log_in_view_model.swift
 //  HopSpot.
 //
-//  Created by Ben Roman on 2024-07-10.
+//  Created by Mina Mansour  on 2024-07-10.
 //
 
 import Foundation
+import Firebase
 
 class log_in_view_model: ObservableObject{
+    @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
     
-    @Published var email = ""
-    @Published var password = ""
-    @Published var errormessage = ""
     
-    init(){}
     
-    func login(){
-        //makes sure that email and password is not blank
-        guard !email.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty, !password.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-        else{
-            errormessage = "Please fill out all fields"
-            return
-        }
-        //hello
+    init(){
+        
+    }
+    func signIn(withEmail email: String, password: String) async throws {
         
     }
     
-    func validate(){
+    func createUser(withEmail email: String, password: String, fullname: String) async throws {
         
     }
     
-    
+    func signOut(){
+        
+    }
+    func deleteAccount(){
+        
+    }
+    func fetchUser(){
+        
+    }
 }
