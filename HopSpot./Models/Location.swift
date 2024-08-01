ced73f4d789e86614a386355267720537ce07c43
Benjamin Roman Perez <59741853+roman20203@users.noreply.github.com>
2024-08-01 19:58:43 -0400

    THIS BETTER FIX EVERYTHING

---
+++ HopSpot./Models/Location.swift
@@ -0,0 +1,28 @@
+//
+//  Location.swift
+//  HopSpot.
+//
+//  Created by mina on 2024-07-26.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+
+struct Location: Identifiable, Equatable {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    var id: String {
+        name + cityName
+    }
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
