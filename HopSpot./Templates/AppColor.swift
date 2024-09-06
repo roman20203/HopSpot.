a29f374f95edb294535dcff2af1b0ad17d2b5b5f
Benjamin Roman Perez <59741853+roman20203@users.noreply.github.com>
2024-09-05 20:49:47 -0400

    Updated App Color and Apple Icon

--- HopSpot./Templates/AppColor.swift
+++ HopSpot./Templates/AppColor.swift
@@ -8,5 +8,6 @@
 import SwiftUI
 
 struct AppColor {
-    static let color = Color(.systemPink)
+    static let color = Color(red: 237/255, green: 32/255, blue: 124/255, opacity: 255/255)
 }
+//rgba(237,32,124,255)
